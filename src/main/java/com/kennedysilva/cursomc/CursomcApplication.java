8e19090e58a24aa56c17ad5b4f46735d4849b919
Kennedy <kennedysilva20081@gmail.com>
2021-03-07 20:08:44 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/kennedysilva/cursomc/CursomcApplication.java
+++ src/main/java/com/kennedysilva/cursomc/CursomcApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kennedysilva.cursomc.domain.Categoria;
+import com.kennedysilva.cursomc.domain.Produto;
 import com.kennedysilva.cursomc.repositories.CategoriaRepository;
+import com.kennedysilva.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -27,7 +32,21 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 		
 		
 	}
